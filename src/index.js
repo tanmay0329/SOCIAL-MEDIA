0aa341bd03aad225e4debb1daa574c1dfd6247c0
Tanmay <tanmay.bora221@vit.edu>
2025-12-10 15:54:21 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -8,8 +8,15 @@ dotenv.config({
 });
 
 
-
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port: ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log("Error connecting to DB in index.js:", err);
+});
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiRespone{
+    constructor(statusCode, message="Success", data={}){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode < 400;
+    }
+}
+
+export default ApiRespone;
