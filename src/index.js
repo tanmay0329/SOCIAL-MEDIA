d4039d81ba4b6e02afa838d12e60c61916651ff1
Tanmay <tanmay.bora221@vit.edu>
2025-12-10 15:15:52 +0530

    CONNECT TO MONGO ATLAS

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+// (async() => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//         app.on("error", (error) => {
+//             console.log("Express server error:", error);
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     } catch(error){
+//         console.log("MongoDB connection error:", error);
+//     }
+// })();
